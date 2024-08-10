685bb2cd1cb5ce993abb843be3165b4fd0205b2a
Gaëtan de Villèle <gdevillele@proton.me>
2024-08-10 12:36:57 +0200

    update run tool 0.1.1 (175)

--- epicgamesstore/runTool.sh
+++ epicgamesstore/runTool.sh
@@ -3,7 +3,7 @@
 # exit when any command fails
 set -e
 
-CUBZH_RELEASE_BUILD_VERSION="0.0.67_165"
+CUBZH_RELEASE_BUILD_VERSION="0.1.1_175"
 
 SCRIPT_PATH=`readlink -f "${BASH_SOURCE:-$0}"`
 SCRIPT_DIR=`dirname ${SCRIPT_PATH}`
